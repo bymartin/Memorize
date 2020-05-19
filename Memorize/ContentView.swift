b2169d3889c28ea2e6c88a5d5b08beb6fb4219fc
Barry Martin <bymartin@users.noreply.github.com>
2020-05-19 16:13:46 -0500

    Stanford CS193p Spring 2020 Lecture One https://youtu.be/jbtqIBpUG7g

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,9 +10,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
     }
 }
+
+struct CardView : View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
