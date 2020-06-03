e2e25092152d661c8618cc357d743b9b40c0adf3
Barry Martin <bymartin@users.noreply.github.com>
2020-06-02 21:22:12 -0500

    Add custom shape Pie

--- Memorize/Pie.swift
+++ Memorize/Pie.swift
@@ -18,12 +18,13 @@ struct Pie: Shape {
         let center = CGPoint(x: rect.midX, y: rect.midY)
         let radius = min(rect.width, rect.height) / 2
         let start = CGPoint(
-            x: center.x + radius + cos(CGFloat(startAngle.radians)),
-            y: center.y + radius + sin(CGFloat(startAngle.radians))
+            x: center.x + radius * cos(CGFloat(startAngle.radians)),
+            y: center.y + radius * sin(CGFloat(startAngle.radians))
         )
         
         var p = Path()
         p.move(to: center)
+        p.addLine(to: start)
         p.addArc(center: center,
                  radius: radius,
                  startAngle: startAngle,
@@ -35,4 +36,10 @@ struct Pie: Shape {
     }
     
     
+}
+
+struct Pie_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
 }
