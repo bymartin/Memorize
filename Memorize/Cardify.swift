85e018d7d0cfc9c37c5c85fc32631477300ba1b9
Barry Martin <bymartin@users.noreply.github.com>
2020-06-07 00:37:34 -0500

    End of Lecture 6 on Animation

--- Memorize/Cardify.swift
+++ Memorize/Cardify.swift
@@ -8,20 +8,36 @@
 
 import SwiftUI
 
-struct Cardify: ViewModifier {
-    var isFaceUp: Bool
+struct Cardify: AnimatableModifier {
+    var rotation: Double
+    
+    init(isFaceUp: Bool) {
+        rotation = isFaceUp ? 0 : 180
+    }
+    
+    var isFaceUp: Bool {
+        rotation < 90
+    }
+    
+    var animatableData: Double {
+        get { return rotation }
+        set { rotation = newValue }
+    }
     
     func body(content: Content) -> some View {
         
         ZStack {
-            if isFaceUp {
+            Group {
                 RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
                 RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
                 content
-            } else {
-                RoundedRectangle(cornerRadius: cornerRadius).fill()
             }
+            .opacity(isFaceUp ? 1 : 0)
+            
+            RoundedRectangle(cornerRadius: cornerRadius).fill()
+            .opacity(isFaceUp ? 0 : 1)
         }
+        .rotation3DEffect(Angle.degrees(rotation), axis: (0,1,0))
     }
     
     private let cornerRadius: CGFloat = 10
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -12,27 +12,46 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        NavigationView {
-            VStack {
-                Grid(viewModel.cards) { card in
-                    CardView(card: card).onTapGesture  {
+
+        VStack {
+            Grid(viewModel.cards) { card in
+                CardView(card: card).onTapGesture  {
+                    withAnimation(.linear) {
                         self.viewModel.choose(card: card)
                     }
-                    .padding(5)
                 }
-                .padding()
-                .foregroundColor(viewModel.color)
-                .navigationBarTitle(Text("Memorize! - \(viewModel.themeName)"), displayMode: .inline)
-                .navigationBarItems(trailing:
-                    Button(action: self.viewModel.newGame) {
-                        Text("New Game")
-                })
-                
-                Text("Score: \(self.viewModel.score)")
-                    .bold()
-                    .foregroundColor(self.viewModel.color)
+                .padding(5)
             }
+            .padding()
+            .foregroundColor(viewModel.color)
+            //.navigationBarTitle(Text("Memorize! - \(viewModel.themeName)"), displayMode: .inline)
+            //.navigationBarItems(trailing:
+            //    Button(action:
+            //    withAnimation(.easeInOut) { self.viewModel.newGame }) {
+            //        Text("New Game")
+            //})
+            
+            HStack {
+                Text("Theme: \(viewModel.themeName)")
+                Spacer()
+                
+                Button(action: {
+                    withAnimation(.easeInOut) {
+                        self.viewModel.newGame()
+                    }
+                    }) {
+                    Text("New Game")
+                }
+                
+                Spacer()
+                Text("Score: \(self.viewModel.score)")
+                .bold()
+            }
+            .padding(.leading)
+            .padding(.trailing)
+            .foregroundColor(self.viewModel.color)
         }
+        
         
     }
 }
@@ -46,19 +65,43 @@ struct CardView : View {
         }
     }
     
+    @State private var animatedBonusRemaining: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+            animatedBonusRemaining = 0
+        }
+    }
+    
     @ViewBuilder
     private func body(for size: CGSize) -> some View {
         if card.isFaceUp || !card.isMatched {
             ZStack {
-                // iOS drawing, 0.0 is upper left, and increasing down and right
-                // clockwise backwards
-                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90),
-                    clockwise: true)
-                    .padding(5).opacity(0.4)
+                Group {
+                    if card.isConsumingBonusTime {
+                        // iOS drawing, 0.0 is upper left, and increasing down and right
+                        // clockwise backwards
+                        Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-animatedBonusRemaining*360-90),
+                            clockwise: true)
+                            .onAppear {
+                                self.startBonusTimeAnimation()
+                        }
+                    } else {
+                        Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-card.bonusRemaining*360-90),
+                        clockwise: true)
+                    }
+                }
+                .padding(5).opacity(0.4)
+                
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
+                    // implicit animation
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360: 0))
+                    .animation(card.isMatched ? Animation.linear(duration:0.75).repeatForever(autoreverses: false) : .default)
             }
             .cardify(isFaceUp: card.isFaceUp)
+            .transition(AnyTransition.scale)
         }
 
     }
